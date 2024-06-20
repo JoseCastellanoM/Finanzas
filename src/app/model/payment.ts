df4050ee8bb6938350fe0b27ef33de093891eadd
AndyS-GR <u20201b763@upc.edu.pe>
2024-06-20 18:47:05 -0500

    class: payment class updated

--- src/app/model/payment.ts
+++ src/app/model/payment.ts
@@ -3,5 +3,7 @@ import { Purchase } from "./purchase";
 export class Payment {
   value: number = 0;
   date: Date = new Date(Date.now());
-  purchase: Purchase = new Purchase();
+  purchase_id: number = -1;
+  interest : number = 0;
+  amortization : number = 0;
 }
